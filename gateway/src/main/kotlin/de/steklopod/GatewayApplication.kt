ddc2d8c45efd560f8269db5928ab676bad66d43f
Tsimur <nut.t@icloud.com>
2022-06-15 23:24:07 +0200

    gatevay fils

---
+++ gateway/src/main/kotlin/de/steklopod/GatewayApplication.kt
@@ -0,0 +1,11 @@
+package de.steklopod
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewayApplication>(*args)
+}
